007d9afc48f7b583d24cfdd7e90679b58538ba06
Tomek Kruszyna <tomek.kruszyna@gmail.com>
2020-06-24 21:33:02 +0200

    stworzyłem klasę User

---
+++ src/main/java/com/example/excercise22/User.java
@@ -0,0 +1,13 @@
+package com.example.excercise22;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public User(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
